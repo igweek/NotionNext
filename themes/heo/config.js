691e66f0462bc3389c72d42b43d3519f4a4edfc8
igweek <soscn@hotmail.com>
2024-08-29 23:11:45 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -15,8 +15,8 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '错提收集',
-  HEO_HERO_TITLE_2: '与进化思维',
+  HEO_HERO_TITLE_1: '错题收集',
+  HEO_HERO_TITLE_2: '思维进化',
   HEO_HERO_TITLE_3: 'Steven',
   HEO_HERO_TITLE_4: '我的另一个博客',
   HEO_HERO_TITLE_5: '钵钵鸡',
